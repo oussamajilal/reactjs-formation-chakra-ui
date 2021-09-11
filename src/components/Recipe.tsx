821822a2a8049c73067780b79afb06c45174c8ea
Oussama Jilal <oussama.jilal@maltem.com>
2021-09-11 22:00:37 +0100

    Added state & event

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,9 +1,20 @@
+import { Box, Button } from "@chakra-ui/react";
 import * as React from "react"
 
 interface RecipeProps {
     id: number;
 }
 
-const Recipe = (props: RecipeProps) => <span>My Recipe N° {props.id}</span>
+const Recipe = (props: RecipeProps) => {
+    const [inc, setInc] = React.useState(0);
+    const handleIncClick = () => {
+        setInc(inc + 1);
+    };
+
+    return <Box>
+        <span>My Recipe N° {props.id} incremented {inc} times</span>
+        <Button onClick={handleIncClick}>+</Button>
+    </Box>
+}
 
 export default Recipe;
