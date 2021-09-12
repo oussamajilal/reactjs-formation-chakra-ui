548e6184f9841a38244806ae232bfded5cb5206e
Oussama Jilal <oussama.jilal@maltem.com>
2021-09-12 18:21:17 +0100

    Added formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  theme,
-} from "@chakra-ui/react"
-import Menu from "./components/Menu"
+import * as React from "react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import Menu from "./components/Menu";
+import ReceipeForm from "./components/ReceipeForm";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Menu />
+    <hr />
+    <ReceipeForm />
   </ChakraProvider>
-)
+);
