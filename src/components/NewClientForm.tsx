a5c21f5117d5862a9462fdca013e362b4301e334
Oussama Jilal <oussama.jilal@maltem.com>
2021-09-14 16:48:56 +0100

    Added FormErrorMessage

--- src/components/NewClientForm.tsx
+++ src/components/NewClientForm.tsx
@@ -13,7 +13,11 @@ import {
   NumberInputStepper,
 } from "@chakra-ui/number-input";
 import { Switch } from "@chakra-ui/switch";
-import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+} from "@chakra-ui/form-control";
 import { EmailIcon } from "@chakra-ui/icons";
 import { FormikProps } from "formik";
 import { Client } from "./Client";
@@ -25,79 +29,106 @@ interface NewClientFormProps {
 const NewClientForm: React.FC<NewClientFormProps> = (props) => {
   return (
     <>
-      <Input
-        placeholder="ID"
+      <FormControl
         mb="2"
-        {...props.formik.getFieldProps("id")}
         isInvalid={!!(props.formik.touched.id && props.formik.errors.id)}
-      />
-      <Input
-        placeholder="First Name"
+      >
+        <Input placeholder="ID" {...props.formik.getFieldProps("id")} />
+        <FormErrorMessage>{props.formik.errors.id}</FormErrorMessage>
+      </FormControl>
+      <FormControl
         mb="2"
-        {...props.formik.getFieldProps("firstName")}
         isInvalid={
           !!(props.formik.touched.firstName && props.formik.errors.firstName)
         }
-      />
-      <Input
-        placeholder="Last Name"
+      >
+        <Input
+          placeholder="First Name"
+          {...props.formik.getFieldProps("firstName")}
+        />
+        <FormErrorMessage>{props.formik.errors.firstName}</FormErrorMessage>
+      </FormControl>
+      <FormControl
         mb="2"
-        {...props.formik.getFieldProps("lastName")}
         isInvalid={
           !!(props.formik.touched.lastName && props.formik.errors.lastName)
         }
-      />
-      <InputGroup mb="2">
-        <InputLeftElement
-          pointerEvents="none"
-          children={<EmailIcon color="gray.300" />}
-        />
+      >
         <Input
-          type="email"
-          placeholder="Email"
-          {...props.formik.getFieldProps("email")}
-          isInvalid={
-            !!(props.formik.touched.email && props.formik.errors.email)
-          }
+          placeholder="Last Name"
+          {...props.formik.getFieldProps("lastName")}
         />
-      </InputGroup>
-      <InputGroup mb="2">
-        <InputLeftAddon children="+212" />
-        <Input
-          type="tel"
-          placeholder="Phone Number"
-          {...props.formik.getFieldProps("phoneNumber")}
-          isInvalid={
-            !!(
-              props.formik.touched.phoneNumber &&
-              props.formik.errors.phoneNumber
-            )
-          }
-        />
-      </InputGroup>
-      <NumberInput
+        <FormErrorMessage>{props.formik.errors.firstName}</FormErrorMessage>
+      </FormControl>
+      <FormControl
         mb="2"
-        defaultValue={props.formik.getFieldProps("height").value}
-        onChange={props.formik.getFieldProps("height").onChange}
-        onBlur={props.formik.getFieldProps("height").onChange}
-        precision={2}
-        step={0.1}
-        max={100}
-        min={20}
+        isInvalid={!!(props.formik.touched.email && props.formik.errors.email)}
+      >
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<EmailIcon color="gray.300" />}
+          />
+          <Input
+            type="email"
+            placeholder="Email"
+            {...props.formik.getFieldProps("email")}
+          />
+        </InputGroup>
+        <FormErrorMessage>{props.formik.errors.email}</FormErrorMessage>
+      </FormControl>
+      <FormControl
+        mb="2"
+        isInvalid={
+          !!(
+            props.formik.touched.phoneNumber && props.formik.errors.phoneNumber
+          )
+        }
+      >
+        <InputGroup>
+          <InputLeftAddon children="+212" />
+          <Input
+            type="tel"
+            placeholder="Phone Number"
+            {...props.formik.getFieldProps("phoneNumber")}
+          />
+        </InputGroup>
+        <FormErrorMessage>{props.formik.errors.phoneNumber}</FormErrorMessage>
+      </FormControl>
+      <FormControl
+        mb="2"
         isInvalid={
           !!(props.formik.touched.height && props.formik.errors.height)
         }
       >
-        <NumberInputField
-          placeholder="Height"
-          {...props.formik.getFieldProps("height")}
-        />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
-      <FormControl display="flex" alignItems="center">
+        <NumberInput
+          mb="2"
+          defaultValue={props.formik.getFieldProps("height").value}
+          onChange={props.formik.getFieldProps("height").onChange}
+          onBlur={props.formik.getFieldProps("height").onChange}
+          precision={2}
+          step={0.1}
+          max={100}
+          min={20}
+        >
+          <NumberInputField
+            placeholder="Height"
+            {...props.formik.getFieldProps("height")}
+          />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+        <FormErrorMessage>{props.formik.errors.height}</FormErrorMessage>
+      </FormControl>
+      <FormControl
+        display="flex"
+        alignItems="center"
+        isInvalid={
+          !!(props.formik.touched.isActive && props.formik.errors.isActive)
+        }
+      >
         <FormLabel htmlFor="client-active" mb="0">
           Active?
         </FormLabel>
@@ -106,6 +137,7 @@ const NewClientForm: React.FC<NewClientFormProps> = (props) => {
           isChecked={props.formik.getFieldProps("isActive").value}
           {...props.formik.getFieldProps("isActive")}
         />
+        <FormErrorMessage>{props.formik.errors.isActive}</FormErrorMessage>
       </FormControl>
     </>
   );
