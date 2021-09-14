ed7ff147016298ac02e64811d6a1156d4961aa01
Oussama Jilal <oussama.jilal@maltem.com>
2021-09-14 23:13:08 +0100

    Added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { ChakraProvider, theme } from "@chakra-ui/react";
 import ClientList from "./components/Client/ClientList";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import Profile from "./components/Profile/Profile";
 
 const queryClient = new QueryClient();
 
@@ -10,7 +12,16 @@ export const App = () => (
   <QueryClientProvider client={queryClient}>
     <ChakraProvider theme={theme}>
       <ColorModeSwitcher />
-      <ClientList />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/">
+            <ClientList />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </ChakraProvider>
   </QueryClientProvider>
 );
