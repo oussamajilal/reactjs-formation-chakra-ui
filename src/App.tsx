33cf04d963374e616fe80d0331d9edf2aec04a6d
Oussama Jilal <oussama.jilal@maltem.com>
2021-09-14 23:26:02 +0100

    Added Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
 import * as React from "react";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  ChakraProvider,
+  Flex,
+  theme,
+  Spacer,
+  IconButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import ClientList from "./components/Client/ClientList";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Link } from "react-router-dom";
 import Profile from "./components/Profile/Profile";
+import { DragHandleIcon, HamburgerIcon, StarIcon } from "@chakra-ui/icons";
 
 const queryClient = new QueryClient();
 
 export const App = () => (
   <QueryClientProvider client={queryClient}>
     <ChakraProvider theme={theme}>
-      <ColorModeSwitcher />
       <BrowserRouter>
+        <Flex>
+          <Spacer />
+          <ColorModeSwitcher />
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<HamburgerIcon />}
+              variant="outline"
+            />
+            <MenuList>
+              <Link to="/">
+                <MenuItem icon={<DragHandleIcon />}>Home Page</MenuItem>
+              </Link>
+              <Link to="/profile">
+                <MenuItem icon={<StarIcon />}>Profile</MenuItem>
+              </Link>
+            </MenuList>
+          </Menu>
+        </Flex>
         <Switch>
           <Route path="/profile">
             <Profile />
